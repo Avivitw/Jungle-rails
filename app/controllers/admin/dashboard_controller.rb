09745555b8241c8a79d29a10b10964e0e2229652
Avivit Weissman <wavivit@gmail.com>
2021-06-23 22:09:45 -0700

    Adede dashboard display

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,5 +4,7 @@ class Admin::DashboardController < ApplicationController
   
   http_basic_authenticate_with name: ENV['username'], password: ENV['password']
   def show
+    @products_count = Product.count
+    @categories = Category.count
   end
 end
