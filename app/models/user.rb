427b80a38857d35e3a6509944995dcf928442c04
Avivit Weissman <wavivit@gmail.com>
2021-06-28 22:57:41 -0700

    added New authentication (class) method and tests for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,20 @@ class User < ActiveRecord::Base
   validates :password, presence: { on: create }, length: { minimum: 8 }
   validates :password_confirmation, presence: { on: create }, length: { minimum: 8 }
 
+  def self.authenticate_with_credentials(email, password)
+    # if successfully authenticated return instance of the user
+    # else return nill
+    user = User.find_by_email(email)
+      # If the user exists AND the password entered is correct.
+      if user && user.authenticate(password)
+        # Save the user id inside the browser cookie. This is how we keep the user 
+        # logged in when they navigate around our website.
+        user
+      else
+        nil
+      end
+    
+  end
+
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe User, type: :model do
       it 'should create a user and save a valid password and password_confirmation' do
           @user = User.create({
           name: "vivian",
-          email: "vivi@example.com",
+          email: "viviian@example.com",
           password: "12345678",
           password_confirmation: "12345678"
         })
@@ -18,7 +18,7 @@ RSpec.describe User, type: :model do
           name: "bobo",
           email: "bobo@example.com",
           password: "12345678",
-          password_confirmation: "1111"
+          password_confirmation: "11111111"
         })
         expect(@user).not_to be_valid
         puts @user.errors.full_messages
@@ -33,7 +33,7 @@ RSpec.describe User, type: :model do
         puts @user.errors.full_messages
       end
      
-      it 'should fail - not create a user when password_confirmation if one is missing' do
+      it 'should fail - not create a user when password_confirmation is missing' do
           @user = User.create({
           name: "koko",
           email: "koko@example.com",
@@ -43,7 +43,7 @@ RSpec.describe User, type: :model do
         puts @user.errors.full_messages
       end
 
-      it 'should fail - not create a user when password if one is missing' do
+      it 'should fail - not create a user when password is missing' do
           @user = User.create({
           name: "nono",
           email: "nono@example.com",
@@ -65,5 +65,41 @@ RSpec.describe User, type: :model do
       end
 
   end
+
+
+  describe '.authenticate_with_credentials' do
+
+    before(:all)  do
+          user = User.create({
+          name: "rachel",
+          email: "rachel@example.com",
+          password: "22222222",
+          password_confirmation: "22222222"
+        })
+      end
+
+    it 'should fail - not create a user instance when password is wrong' do
+      user = User.authenticate_with_credentials("vivi@example.com", "11111111")
+      expect(user).to be_nil
+    end
+
+    it 'should fail - not create a user instance when email does not exist' do
+      user = User.authenticate_with_credentials("fake@nono.com", "11111111")
+      expect(user).to be_nil
+    end
+
+    it 'should fail - not create a user instance when password is empty' do
+      user = User.authenticate_with_credentials("vivi@example.com", "")
+      expect(user).to be_nil
+    end
+
+    it 'should pass - create a user instance when email and password are correct' do
+      user = User.authenticate_with_credentials("rachel@example.com", "12345678")
+      expect(user.name).to eq "rachel"
+    end
+    
+
+  end
+
   
 end
